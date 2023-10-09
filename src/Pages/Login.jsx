5505532a17e0ce11727fbde1da92e3314437728d
swapno92 <swapnoprograming92@gmaiil.com>
2023-10-09 20:56:29 +0600

    assignment recap and something change

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -43,33 +43,35 @@ const Login = () => {
     }
 
     return (
-        <div className="border border-gray-300 my-5 rounded-lg pb-8 lg:w-1/2 mx-4 lg:mx-auto   lg:px-0 md:px-10 flex flex-col items-center">
-            <h2 className="text-3xl my-10 text-center font-bold" data-aos="fade-down">This is Login</h2>
-            <form onSubmit={handleLogin} className="md:w-3/4 lg:w-1/2 mx-auto" data-aos="fade-down">
-                <div className="form-control">
-                    <label className="label">
-                        <span className="label-text font-semibold text-md">Email</span>
-                    </label>
-                    <input type="email" required name="email" placeholder="Email" className="input input-bordered"  />
-                </div>
-                <div className="form-control">
-                    <label className="label">
-                        <span className="label-text font-semibold text-md">Password</span>
-                    </label>
-                    <input type="password" required name="password" placeholder="Password" className="input input-bordered"  />
-                    <label className="label">
-                        <a href="#" className="label-text-alt link link-hover font-semibold text-md">Forgot password?</a>
-                    </label>
-                    {
-                        error && <p className="font-semibold text-sm text-red-400">{error}</p>
-                    }
-                </div>
-                <div className="form-control mt-6">
-                    <button className="btn btn-primary">Login</button>
-                </div>
-                <p className="text-center mt-4">Do not have an account <Link className="text-blue-600 font-bold" to='/register'>Register</Link></p>
-            </form>
-            <button onClick={handleWithGoogle} className=" bg-slate-300 px-16 py-2 rounded-lg border border-lime-600 mt-6 flex items-center gap-2" data-aos="fade-down"><FcGoogle className="text-2xl"></FcGoogle>Sign in with google</button>
+        <div className="border border-gray-300 rounded-lg  lg:w-1/2  mx-auto" >
+            < div className="pb-8 mx-4 lg:mx-auto   lg:px-0 md:px-10 flex flex-col items-center " data-aos="flip-left">
+                <h2 className="text-3xl my-10 text-center font-bold" >This is Login</h2>
+                <form onSubmit={handleLogin} className="md:w-3/4 lg:w-1/2 mx-auto" >
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text font-semibold text-md">Email</span>
+                        </label>
+                        <input type="email" required name="email" placeholder="Email" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text font-semibold text-md">Password</span>
+                        </label>
+                        <input type="password" required name="password" placeholder="Password" className="input input-bordered" />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover font-semibold text-md">Forgot password?</a>
+                        </label>
+                        {
+                            error && <p className="font-semibold text-sm text-red-400">{error}</p>
+                        }
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-primary">Login</button>
+                    </div>
+                    <p className="text-center mt-4">Do not have an account <Link className="text-blue-600 font-bold" to='/register'>Register</Link></p>
+                </form>
+                <button onClick={handleWithGoogle} className=" bg-slate-300 px-16 py-2 rounded-lg border border-lime-600 mt-6 flex items-center gap-2" ><FcGoogle className="text-2xl"></FcGoogle>Sign in with google</button>
+            </div>
         </div>
     );
 };
